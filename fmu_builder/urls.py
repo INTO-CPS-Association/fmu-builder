27dfa643c3edd23276ff6525b9b1e4446ce32db0
FMU Builder <fmubuild@au.dk>
2016-05-29 21:36:53 +0200

    added fmubuilder without tailing slash

--- fmu_builder/urls.py
+++ fmu_builder/urls.py
@@ -9,6 +9,11 @@ urlpatterns = [
         name="upload",
     ),
     url(
+        r"^fmubuilder$",
+        fmu_builder.website.views.UploadView.as_view(),
+        name="upload",
+    ),
+    url(
         r"^fmubuilder/(?P<uuid>[a-z0-9-]{36})/$",
         fmu_builder.website.views.UploadView.as_view(),
         name="result",
