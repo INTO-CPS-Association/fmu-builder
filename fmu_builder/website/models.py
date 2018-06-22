1346d009a4290126aca43cae10752517f9821620
Casper Thule <casper.thule@eng.au.dk>
2018-06-22 12:08:20 +0200

    Added MWE

--- fmu_builder/website/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-import uuid
-
-
-class BuildLog(models.Model):
-    command_output = models.TextField(blank=False)
-    output_file_path = models.TextField(blank=False)
-    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
--- fmu_builder/website/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd /tmp
-zip $1.zip $1
-echo $1.zip
